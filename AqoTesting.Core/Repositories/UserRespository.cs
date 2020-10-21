5d74257f875c16295d0edc492ff33c83dec80def
Jebati <www.den-safonov@mail.ru>
2020-10-21 12:12:33 +0300

    add respository

---
+++ AqoTesting.Core/Repositories/UserRespository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AqoTesting.Shared.Interfaces;
+
+namespace AqoTesting.Core.Repositories
+{
+    public class UserRespository : IUserRespository
+    {
+
+    }
+}
---
+++ AqoTesting.Shared/Interfaces/IUserRespository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AqoTesting.Shared.Interfaces
+{
+    public interface IUserRespository
+    {
+
+    }
+}
