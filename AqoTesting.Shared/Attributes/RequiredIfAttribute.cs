37e39f789c6e23bfbdd2798fa80a4eeb3af75029
Jebati <www.den-safonov@mail.ru>
2020-11-06 01:44:07 +0300

    Added RequiredIfAttribute

---
+++ AqoTesting.Shared/Attributes/RequiredIfAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AqoTesting.Shared.Attributes
+{
+    public class RequiredIfAttribute : RequiredAttribute
+    {
+        private String PropertyName { get; set; }
+        private Object DesiredValue { get; set; }
+
+        public RequiredIfAttribute(String propertyName, Object desiredvalue)
+        {
+            PropertyName = propertyName;
+            DesiredValue = desiredvalue;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            Object instance = context.ObjectInstance;
+            Type type = instance.GetType();
+            Object proprtyValue = type.GetProperty(PropertyName).GetValue(instance, null);
+            if (proprtyValue.ToString() == DesiredValue.ToString())
+            {
+                ValidationResult result = base.IsValid(value, context);
+                return result;
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
