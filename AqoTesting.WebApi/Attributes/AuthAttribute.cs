28f911a2c2c3d47585876fc066804011d8ec4a4f
Jebati <www.den-safonov@mail.ru>
2020-11-05 17:52:32 +0300

    Added AuthAttribute

---
+++ AqoTesting.WebApi/Attributes/AuthAttribute.cs
@@ -0,0 +1,19 @@
+﻿using AqoTesting.Shared.Enums;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AqoTesting.WebApi.Attributes
+{
+    public class AuthAttribute : AuthorizeAttribute
+    {
+        private Role role;
+        public Role Role
+        {
+            get { return role; }
+            set { role = value; base.Roles = value.ToString(); }
+        }
+    }
+}
